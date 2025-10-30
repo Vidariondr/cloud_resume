330148ade43f44dc572cbdb862c4e0f50d02ffde
J <204088010+Vidariondr@users.noreply.github.com>
2025-10-30 11:09:33 +0100

    add e2e tests

---
+++ test_e2e.py
@@ -0,0 +1,15 @@
+import re
+from playwright.sync_api import Page, expect
+
+def test_has_title(page: Page):
+    page.goto("https://jkaszczuk.com")
+    expect(page).to_have_title(re.compile("Jakub Kaszczuk - Resume"))
+
+def test_visitor_counter_value_changes(page: Page):
+    page.goto("https://jkaszczuk.com")
+    initial_value = int(page.locator("#counter").inner_text())
+
+    page.goto("https://jkaszczuk.com")
+    changed_value = page.locator("#counter")
+    
+    expect(changed_value).to_have_text(str(initial_value + 1))
