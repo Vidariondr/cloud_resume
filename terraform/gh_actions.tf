d3aa6fae78d360df8a5b05d4941693151a0bdc65
J <204088010+Vidariondr@users.noreply.github.com>
2025-11-08 15:22:24 +0100

    add github actions role

---
+++ terraform/gh_actions.tf
@@ -0,0 +1,47 @@
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+}
+
+data "aws_iam_policy_document" "github_actions_policy" {
+  statement {
+    sid    = "AllowGithubAccess"
+    effect = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts:amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:vidariondr/cloud-resume:ref:refs/heads/main"]
+    }
+  }
+  version = "2008-10-17"
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "GitHubActionsTerraform"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
